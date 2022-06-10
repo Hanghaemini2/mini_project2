409c5faa33c8a0016c5b7dba5a54def9282065e9
Frost <tolerate0407@gmail.com>
2022-06-10 21:12:36 +0900

    서머리

--- src/Login.js
+++ src/Login.js
@@ -12,7 +12,11 @@ function Login() {
       <div className="Login_Wrap">
         <div className="Login_Top">
           <div className="GuideBanner">
-
+            <span>로그인</span>
+          </div>
+          <div className="Login_inputWrap">
+            <input className="Login_Input" placeholder="ID를 입력해 주세요"/>
+            <input className="Login_Input" placeholder="비밀번호를 입력해 주세요"/>
           </div>
         </div>
         <div className="Login_Bottom">
