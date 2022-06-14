f78e1bb190f3b40ab36d79087be90597d8eaf964
treblabin <treblabin@gmail.com>
2022-06-14 11:11:14 +0900

    give state to modal

--- src/Main.js
+++ src/Main.js
@@ -2,28 +2,36 @@ import React, { useState } from "react";
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
 import { loadBookAxios } from "./redux/modules/book";
+import { useParams, useNavigate } from "react-router-dom";
 
 import Thumb from "./image/hand-thumbs-up.svg";
 import next from "./image/next_button.svg";
 import prev from "./image/prev_button.svg";
-import Cover01 from "./image/book_sample.jpeg";
 import Detail from "./Detail";
 
 function Main() {
   const dispatch = useDispatch();
+  const param = useParams();
+  const navigate = useNavigate();
 
   const cardInfo = useSelector((state) => state.book.list);
   const cardLists = useSelector((state) => state.book.list.content);
+  const totalPages = useSelector((state) => state.book.list.totalPages);
 
   const [modal, setModal] = useState(false);
+  const [pageViewNum, setPageViewNum] = useState(
+    param.pageNum === undefined ? 0 : param.pageNum
+  );
+  const [modalId, setModalId] = useState(null);
 
+  console.log(param.pageNum);
   React.useEffect(() => {
     dispatch(loadBookAxios());
   }, []);
 
   return (
     <div className="Card_field">
-      {modal === true ? <Detail close={setModal} /> : null}
+      {modal === true ? <Detail close={setModal} id={modalId} /> : null}
       {cardLists === undefined
         ? null
         : cardLists.map((list, i) => {
@@ -41,6 +49,7 @@ function Main() {
                         <button
                           className="book_detailButton"
                           onClick={() => {
+                            setModalId(list.id);
                             setModal(true);
                           }}
                         >
@@ -64,15 +73,64 @@ function Main() {
           })}
       <div className="Bottom_Numbering">
         <div className="page">
-          <img src={prev} className="PageArrow" />
-          <button className="Page_Number" onClick={() => () => {}}>
-            1
+          <img
+            src={prev}
+            className="PageArrow"
+            onClick={() => {
+              setPageViewNum(pageViewNum - 5);
+            }}
+            style={{ display: pageViewNum < 3 ? "none" : "" }}
+          />
+          <button
+            className="Page_Number"
+            onClick={() => {
+              navigate(`/${pageViewNum - 1}`);
+            }}
+            style={{ display: pageViewNum < 2 ? "none" : "" }}
+          >
+            {pageViewNum - 1}
           </button>
-          <button className="Page_Number">2</button>
-          <button className="Page_Number">3</button>
-          <button className="Page_Number">4</button>
-          <button className="Page_Number">5</button>
-          <img src={next} className="PageArrow" />
+          <button
+            className="Page_Number"
+            onClick={() => {
+              navigate(`/${pageViewNum}`);
+            }}
+            style={{ display: pageViewNum < 1 ? "none" : "" }}
+          >
+            {pageViewNum}{" "}
+          </button>
+          <button
+            className="Page_Number"
+            onClick={() => {
+              navigate(`/${pageViewNum + 1}`);
+            }}
+          >
+            {pageViewNum + 1}
+          </button>
+          <button
+            className="Page_Number"
+            onClick={() => {
+              navigate(`/${pageViewNum + 2}`);
+            }}
+          >
+            {pageViewNum + 2}
+          </button>
+          <button
+            className="Page_Number"
+            onClick={() => {
+              navigate(`/${pageViewNum + 3}`);
+            }}
+          >
+            {pageViewNum + 3}
+          </button>
+          <img
+            src={next}
+            className="PageArrow"
+            onClick={() => {
+              setPageViewNum(pageViewNum + 5);
+            }}
+            style={{ display: pageViewNum + 3 > totalPages ? "none" : "" }}
+          />
         </div>
       </div>
     </div>
